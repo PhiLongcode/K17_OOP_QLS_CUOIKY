e6ecbcc3160c48b21ba507a1413f8b41095d8ae8
LowjSee-Rill <ltrannguyendac@gmail.com>
2024-12-26 00:33:32 +0700

    LocTran

--- src/ui/MenuCUI.java
+++ src/ui/MenuCUI.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import controller.InDSSachControl;
 import controller.XoaSachControl;
 import dao.ThemSachMoiDAOFile;
+import entity.Sach;
 
 public class MenuCUI {
 	//fields
@@ -13,6 +14,7 @@ public class MenuCUI {
 		private Scanner keyBoardInput = null;
 		private String prompt;
 		private String command;
+		private String chon;
 		private ThemSachInputCUI themSachInputCUI = null;
 		private InDSSachControl inDSSachControl = null;
 		private XoaSachControl xoaSachControl = null;
@@ -40,6 +42,7 @@ public class MenuCUI {
 			keyBoardInput = _keyBoardInput;
 			prompt = "->";
 			command = " ";
+			chon = "";
 		}
 
 		public MenuCUI(PrintWriter _screenOutput, Scanner _keyBoardInput,
@@ -81,6 +84,10 @@ public class MenuCUI {
 					xoaSach();
 					continue;
 				}
+				if ("5".equalsIgnoreCase(command)) {
+					thanhTien();
+					continue;
+				}
 
 			}
 
@@ -92,20 +99,63 @@ public class MenuCUI {
 
 	private void inDSSach() {
 			inDSSachControl.inDSSach();
-		}
+	}
 
-		private void themSach() {
-			//đối tượng ????UI để người dùng nhập
-			//thông tin của sách
-			themSachInputCUI.nhapThongTinSach();
+	private void themSach() {
+		//đối tượng ????UI để người dùng nhập
+		//thông tin của sách
+		themSachInputCUI.nhapThongTinSach();
 
+	}
+		
+		private void thanhTien() {
+			while (true) {
+				screenOutput.println("Chon mot trong cac lua chon sau:");
+				screenOutput.println("1. Tinh tong thanh tien cua tat ca cac sach dang co");
+				screenOutput.println("2. Tinh tong thanh tien cua tung loai sach");
+				screenOutput.println("3. Tinh trung binh cong theo don gia cua tung loai sach");
+				screenOutput.println("4. Thoat");
+				screenOutput.print(prompt);screenOutput.flush();
+				chon = keyBoardInput.nextLine().trim();
+	
+				switch (chon) {
+					case "1":
+						tinhTongThanhTienTatCa();
+						break;
+					case "2":
+						tinhTongThanhTienTheoLoai();
+						break;
+					case "3":
+						tinhTrungBinhCongDonGia();
+						break;
+					case "4":
+						screenOutput.println("Dang thoat...");
+						return;
+					default:
+						screenOutput.println("Vui long chon 1 trong 4 lua chon tren !!!");
+				}
+			}
+			
 		}
+		private void tinhTongThanhTienTatCa() {
+        
+   		}
+
+		private void tinhTongThanhTienTheoLoai() {
+        
+   		}
+
+		private void tinhTrungBinhCongDonGia() {
+        
+   		}
+
 
 		private void menu() {
 			screenOutput.println("~~~~~~~~~~Console Help Menu~~~~~~~~~");
 			screenOutput.println("[1] - Them moi sach");
 			screenOutput.println("[2] - In danh sach sach");
 			screenOutput.println("[3] - Xoa sach theo ma sach");
+			screenOutput.println("[5] - Tinh tien");
 			screenOutput.println("~~~~~~~~~~Console Help Menu~~~~~~~~~");
 		}
 
